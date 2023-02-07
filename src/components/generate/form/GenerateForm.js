c39700694b1a5f48b4d04fe916bde033f1ce9114
unknown <alan.a.n.r.ar@gmail.com>
2023-02-07 15:19:42 -0300

    new changes

--- src/components/generate/form/GenerateForm.js
+++ src/components/generate/form/GenerateForm.js
@@ -113,7 +113,7 @@ function GenerateForm() {
         <div className='h-[100vh] absolute top-0 w-[100vw] bg-[rgba(0,0,0,0.8)] flex flex-col items-center justify-center'>
           <h3>{correctAnswers}/6</h3>
           <h2>{(correctAnswers > 3) ? `Congratulations, you win!` : `Game over`}</h2>
-          <Button title={'finish'} handleChange={handleEnd} />
+          <Button title={'finish'} handleChange={handleEnd} className={className} />
         </div>
       </div>) : ('')}
 
