f4a6beef94375267a44b7e011acab0efc20459f3
unknown <alan.a.n.r.ar@gmail.com>
2023-02-07 15:11:32 -0300

    new changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header({title}) {
   return (
-    <div className='h-[60px] bg-[#9147ff] w-full flex border-b-[6px] border-[#c197ff]'>
+    <div className='h-[60px] bg-[rgba(158,197,255,0.2)] border-[rgb(158,197,255)] w-full flex border-b-[4px] '>
         <div className='flex flex-row text-white w-[50%] h-full ml-20 items-center'>
             <h1 className='text-[2rem] title'>{title}</h1>
         </div>
