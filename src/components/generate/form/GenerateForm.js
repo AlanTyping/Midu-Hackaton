5bd77dc089b5dc276f6bdf1a0b000e9931dcb540
unknown <alan.a.n.r.ar@gmail.com>
2023-02-07 16:03:53 -0300

    new changes

--- src/components/generate/form/GenerateForm.js
+++ src/components/generate/form/GenerateForm.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { GenerateData } from '../../../API/cohere';
 import { callApi } from '../../../API/openAI';
 import Button from '../../Button.js';
-import loading from './loading.gif';
+import copy from './copy.png';
 
 function GenerateForm() {
   const [waitingState, setWaitingState] = useState(false);
@@ -116,8 +116,9 @@ function GenerateForm() {
           <Button title={'finish'} handleChange={handleEnd} className={className} />
         </div>
       </div>) : ('')}
-
-      <div className='flex w-[80%] items-center justify-end text-[1.2rem]'>answer: {decisions}/6</div>
+      <div className='w-[80%] flex items-center justify-end m-[20px] ml-[0px]'>
+        <h3 className='text-[1.3rem] p-[5px] '>answer: {decisions}/6</h3>
+      </div>
       {waitingState ? 
             (<div className='h-[100vh] text-[2rem] absolute top-0 w-[100vw] bg-[rgba(0,0,0,0.8)] flex flex-col items-center justify-center'>
               <h3>Waiting for data</h3>
@@ -127,7 +128,7 @@ function GenerateForm() {
       {showResults ? (
         <form className='h-[100%] w-full flex flex-col items-center'>
           <label className='form-title'>Insert Text</label><br />
-          <input type="text" name="text" value={text.text} onChange={handleChange} className="text-white rounded-[10px] bg-[rgba(158,197,255,0.2)] border-[rgb(158,197,255)] p-[10px] m-[5px] border-2 w-[40%]" /><br />
+          <input autocomplete="off" type="text" name="text" value={text.text} onChange={handleChange} className="text-white rounded-[10px] bg-[rgba(158,197,255,0.2)] border-[rgb(158,197,255)] p-[10px] m-[5px] border-2 w-[40%]" /><br />
           <label className='form-description'>Result</label>
           <textarea type="text" name="token" autoComplete="off" spellCheck="false" value={formValue} rows="3" className="text-white rounded-[15px]  p-[10px] m-[5px] border-2 bg-[rgba(158,197,255,0.2)] border-[rgb(158,197,255)] resize-none w-[50%] h-[500px]" readOnly />
           <div className='flex flex-col justify-center items-center w-full'>
@@ -149,7 +150,7 @@ function GenerateForm() {
         (
           <form onSubmit={handleSubmit} className='h-[100%] w-full flex flex-col items-center'>
             <label className='form-title'>Insert Text</label><br />
-            <input type="text" placeholder='Request something' name="text" value={text.text} onChange={handleChange} className="text-white rounded-[10px] bg-[rgba(158,197,255,0.2)] border-[rgb(158,197,255)] p-[10px] m-[5px] border-2  w-[40%]" /><br />
+            <input autocomplete="off" type="text" placeholder='Request something' name="text" value={text.text} onChange={handleChange} className="text-white rounded-[10px] bg-[rgba(158,197,255,0.2)] border-[rgb(158,197,255)] p-[10px] m-[5px] border-2  w-[40%]" /><br />
             <label className='form-description'>Result</label>
             <textarea type="text" placeholder='Cohere or openAI? Submit for the answer' name="token" autoComplete="off" spellCheck="false" value={formValue} rows="3" className="text-white rounded-[15px] bg-[rgba(158,197,255,0.2)] border-[rgb(158,197,255)] p-[10px] m-[5px] border-2  resize-none w-[50%] h-[50%]" readOnly />
             <button type='submit' className='h-[50px] w-[100px] border-[2px] hover:bg-[rgba(158,197,255,0.2)] border-[rgb(158,197,255)] m-[10px]'>Submit</button>
--- src/components/tokenize/Form.js
+++ src/components/tokenize/Form.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import copy from './copy.png';
 
 function Form({ handleSubmit, handleChange, formValue, text }) {
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = (e) => {
+        try {
+            navigator.clipboard.writeText(formValue);
+            setCopied(true);
+        } catch (err) {
+            console.error('Error al copiar texto: ', err);
+        }
+        e.preventDefault();
+    };
+
     return (
         <form onSubmit={handleSubmit} className='form h-[350px] relative w-[500px] rounded flex justify-center items-center flex-col'>
             <label className='form-title'>{text.title}</label>
-            <input type="text" name="text" placeholder={text.placeholder} onChange={handleChange} className="text-white m-[5px] rounded-[10px] h-[50px] w-[80%] bg-[rgba(197,158,255,0.2)] p-[10px] border-2 border-[rgb(197,158,255)]" /><br />
+            <input autocomplete="off" type="text" name="text" placeholder={text.placeholder} onChange={handleChange} className="text-white m-[5px] rounded-[10px] h-[50px] w-[80%] bg-[rgba(197,158,255,0.2)] p-[10px] border-2 border-[rgb(197,158,255)]" /><br />
             <label className='form-description'>{text.description}</label>
-            <textarea type="text" name="token" autoComplete="off" spellCheck="false" value={formValue} rows="3" 
-            className="text-white rounded-[15px] bg-[rgba(197,158,255,0.2)] p-[10px] m-[5px] border-2 border-[rgb(197,158,255)] resize-none w-[90%] h-[50%]" readOnly />
-            <button type='submit' className='h-[40px] hover:bg-[rgba(197,158,255,0.2)] rounded-[5px] w-[100px] border-[2px] border-[rgb(197,158,255)] m-[5px]'>Submit</button>
+            <textarea type="text" name="token" autoComplete="off" spellCheck="false" value={formValue} rows="3"
+                className="text-white rounded-[15px] bg-[rgba(197,158,255,0.2)] p-[10px] m-[5px] border-2 border-[rgb(197,158,255)] resize-none w-[90%] h-[50%]" readOnly />
+            <div className='w-[100%] relative flex flex-row items-center justify-center'>
+                <button type='submit' className=' h-[40px] hover:bg-[rgba(197,158,255,0.2)] rounded-[5px] w-[100px] border-[2px] border-[rgb(197,158,255)] m-[5px]'>Submit</button>
+                <img className='h-[20px] w-[20px] z-[10] absolute right-40 hover:cursor-pointer' src={copy} onClick={handleCopy}></img>
+                {copied && <p className='absolute right-12'>Texto copiado</p>}
+            </div>
         </form>
     )
 }
