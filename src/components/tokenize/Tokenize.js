f69a64eb096379c07ff37df274a7a583b84ab088
unknown <alan.a.n.r.ar@gmail.com>
2023-02-07 14:06:32 -0300

    responsive aplications

--- src/components/tokenize/Tokenize.js
+++ src/components/tokenize/Tokenize.js
@@ -7,14 +7,14 @@ import Button from '../Button.js';
 function Tokenize({ setTokenize }) {
 
   const handleChange = () => setTokenize((tokenize) => !tokenize);
-  const className = 'absolute left-16 top-4 hover:bg-[rgba(197,158,255,0.2)]'
+  const className = ' hover:bg-[rgba(197,158,255,0.2)]'
 
   return (
-    <div className='h-[100vh] w-[100%] bg-black flex items-center flex-col'>
-      <div className='w-full flex justify-start relative'>
-        <Button title={'home'} handleChange={handleChange} className={className}/>
+    <div className='h-[100vh] w-[100%] bg-black flex items-center flex-col tokenize'>
+      <div className='w-[80%] h-[70px] flex items-center justify-start m-[20px] ml-[0px]'>
+        <Button title={'home'} handleChange={handleChange} className={className} />
       </div>
-      <div className='h-full w-full flex flex-row items-center container'>
+      <div className='h-[100vh] w-full flex flex-row items-center container'>
         <TokenizeForm />
         <DetokenizeForm />
       </div>
