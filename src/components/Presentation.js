0e973fc8792d6eb1195fb8e77c96df7b05ffeef2
unknown <alan.a.n.r.ar@gmail.com>
2023-02-08 23:36:47 -0300

    new commit

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Presentation() {
   return (
-    <div className='h-[100px] w-full flex flex-col items-center mt-[30px]'>
+    <div className='h-[100px] w-full flex flex-col items-center mt-[25px] mb-[5px]'>
         <h2 className='text-[#b483ff] text-[2.3rem] flex w-full justify-center items-center' id='titulo'>Welcome here</h2>
         <h4 className='text-[#d8beff] text-[1.7rem] flex w-full justify-center items-center' id='subtitulo'>Let's gonna check some co:here interesting aplications</h4>
     </div>
